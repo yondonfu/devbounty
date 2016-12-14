416d6ad6d6f0a7290b4ef336ea89e8ca8a3e63a9
Yondon Fu <yondon.fu@gmail.com>
2016-12-13 19:23:55 -0500

    Working tests for contract initiliazation, funding an issue and registering a developer

--- contracts/Repository.sol
+++ contracts/Repository.sol
@@ -1,7 +1,7 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.6;
 
 import "usingOraclize.sol";
-import "github.com/Arachnid/solidity-stringutils/strings.sol";
+import "strings.sol";
 
 contract Repository is usingOraclize {
   using strings for *;
@@ -9,33 +9,32 @@ contract Repository is usingOraclize {
   struct Issue {
     string url;
     uint bounty;
+    bool initialized;
   }
 
   struct PullRequest {
     string url;
     Issue issue;
+    bool initialized;
   }
-
-  enum OraclizeQueryType { OpenPullRequest, MergePullRequest };
-
-  OraclizeQueryType oraclizeQueryType;
-
-  mapping(string => Issue) issues; // issue url => Issue
-
-  mapping(address => uint) public collaterals; // developer address => posted collateral
-
-  mapping(address => PullRequest) public activePullRequests; // developer address => PullRequest
-
-  address activeDev;
 
   string public url;
   uint public minCollateral;
   uint public penaltyNum;
   uint public penaltyDenom;
 
-  function Repository(string _url, uint _minCollateral, uint _penaltyNum, uint _penatlyDenom) {
+  mapping(string => Issue) issues; // issue url => Issue
+  mapping(address => uint) public collaterals; // developer address => posted collateral
+  mapping(address => PullRequest) activePullRequests; // developer address => PullRequest
+
+  address activeDev;
+
+  enum OraclizeQueryType { OpenPullRequest, MergePullRequest, NoPullRequest }
+  OraclizeQueryType oraclizeQueryType;
+
+  function Repository(string _url, uint _minCollateral, uint _penaltyNum, uint _penaltyDenom) {
     // ethereum-bridge
-    OAR = OraclizeAddrResolverI(0x7c2b639ae051fdaeecc2f8692911627020304ae2);
+    OAR = OraclizeAddrResolverI(0xa0f0f1b1ca09a763f9000c46c3b0baedd099fbdb);
 
     url = _url;
     minCollateral = _minCollateral;
@@ -43,32 +42,26 @@ contract Repository is usingOraclize {
     penaltyDenom = _penaltyDenom;
   }
 
-  function __callback(bytes32 myId, string result) {
-    if (msg.sender != oraclize_cbAddress()) throw; // Non-oraclize message
+  function calcPenalty(uint amount) public constant returns(uint) {
+    return (amount * penaltyNum) / penaltyDenom;
+  }
 
-    if (oraclizeQueryType == OraclizeQueryType.OpenPullRequest) {
-      if (bytes(result).length == 0) {
-        // No open pull request
-        collaterals[activeDev] -= calcPenalty(collaterals[activeDev]);
-      } else {
-        createPullRequest(activeDev, result);
-      }
-    } else if (oraclizeQueryType == OraclizeQueryType.MergePullRequest) {
-      if (result.strCompare("false") || bytes(activePullRequests[activeDev]).length == 0) {
-        // Pull request not merged
-        collaterals[activeDev] -= calcPenalty(collaterals[activeDev]);
-      } else {
-        // Pull request merged
-        if (!activeDev.send(issues[activePullRequests.issueUrl].bounty)) throw;
-
-        delete activePullRequests[activeDev];
-      }
+  function fundIssue(string url) payable {
+    if (!issues[url].initialized) {
+      issues[url] = Issue(url, msg.value, true);
     } else {
-      // Unknown oraclize query type
-      throw;
+      issues[url].bounty += msg.value;
     }
+  }
 
-    activeDev = 0x0;
+  function getIssueByUrl(string url) public constant returns(string, uint) {
+    return (issues[url].url, issues[url].bounty);
+  }
+
+  function registerDev() payable {
+    if (msg.value < minCollateral) throw; // Insufficient collateral
+
+    collaterals[msg.sender] = msg.value;
   }
 
   function createPullRequest(address addr, string oraclizeResult) {
@@ -76,39 +69,13 @@ contract Repository is usingOraclize {
     var url = resultSlice.split(",".toSlice()).toString();
     var issueUrl = resultSlice.toString();
 
-    if (issues[issueUrl] == 0x0) throw; // Issue does not exist
+    if (!issues[issueUrl].initialized) throw; // Issue does not exist
 
-    activePullRequests[addr] = PullRequest(url, issues[issueUrl]);
-  }
-
-  function fundIssue(string url, uint id) payable {
-    issues[url].url = url;
-    issues[url].id = id;
-    issues[url].bounty += msg.value;
-  }
-
-  function getIssueByUrl(string url) public constant returns(string, uint, uint) {
-    return (issues[url].url, issues[url].id, issues[url].bounty);
-  }
-
-  function registerDev() payable {
-    if (msg.value < minCollateral) throw; // Insufficient collateral posted
-
-    collaterals[msg.sender] = msg.value;
-  }
-
-  function mergePullRequest(string apiUrl) {
-    if (collaterals[msg.sender] == 0x0) throw; // Not registered developer address
-    if (activePullRequests[msg.sender] == 0x0) throw; // Developer has not opened a pull request
-
-    activeDev = msg.sender;
-    oraclizeQueryType = OraclizeQueryType.MergePullRequest;
-
-    oraclize_query('URL', apiUrl); // Client has to provide the constructed api url i.e. json(url).merged
+    activePullRequests[addr] = PullRequest(url, issues[issueUrl], true);
   }
 
   function openPullRequest(string apiUrl) {
-    if (collaterals[msg.sender] == 0x0) throw; // Not registered developer address
+    if (collaterals[msg.sender] == 0) throw; // Not registered developer address
 
     activeDev = msg.sender;
     oraclizeQueryType = OraclizeQueryType.OpenPullRequest;
@@ -116,7 +83,41 @@ contract Repository is usingOraclize {
     oraclize_query('URL', apiUrl); // Client has to provide the constructed api url - json(url).[url, issue_url]
   }
 
-  function calcPenalty(uint amount) public constant returns(uint) {
-    return (amount * penaltyNum) / penaltyDenom;
+  function mergePullRequest(string apiUrl) {
+    if (collaterals[msg.sender] == 0) throw; // Not registered developer address
+    if (!activePullRequests[msg.sender].initialized) throw; // Developer has not opened a pull request
+
+    activeDev = msg.sender;
+    oraclizeQueryType = OraclizeQueryType.MergePullRequest;
+
+    oraclize_query('URL', apiUrl); // Client has to provide the constructed api url i.e. json(url).merged
   }
+
+  function __callback(bytes32 myId, string result) {
+    if (msg.sender != oraclize_cbAddress()) throw; // Non-oraclize message
+
+    if (oraclizeQueryType == OraclizeQueryType.OpenPullRequest) {
+      if (bytes(result).length == 0) {
+        // Invalid pull request
+        collaterals[activeDev] -= calcPenalty(collaterals[activeDev]);
+      } else {
+        createPullRequest(activeDev, result);
+      }
+    } else if (oraclizeQueryType == OraclizeQueryType.MergePullRequest) {
+      if (strCompare(result, "false") == 0 || !activePullRequests[activeDev].initialized) {
+        // Pull request not merged or no such open pull request
+        collaterals[activeDev] -= calcPenalty(collaterals[activeDev]);
+      } else {
+        if (!activeDev.send(activePullRequests[activeDev].issue.bounty)) throw;
+
+        delete activePullRequests[activeDev];
+      }
+    } else {
+      // No active pull request or unknown oraclize query type
+      throw;
+    }
+
+    activeDev = 0x0;
+  }
+
 }
--- test/repository.js
+++ test/repository.js
@@ -1,11 +1,11 @@
-contract('SmartRepository', function(accounts) {
+contract('Repository', function(accounts) {
   it('should match initial contract settings', function(done) {
     var repoUrl = 'https://github.com/foo/bar';
     var minCollateral = web3.toBigNumber(web3.toWei(1, 'ether')).toNumber();
     var penaltyNum = 15;
     var penaltyDenom = 100;
 
-    SmartRepository.new(repoUrl, minCollateral, penaltyNum, penaltyDenom, { from: accounts[0] }).then(function(repo) {
+    Repository.new(repoUrl, minCollateral, penaltyNum, penaltyDenom, { from: accounts[0] }).then(function(repo) {
       repo.url.call().then(function(url) {
         assert.equal(url, repoUrl, 'repo should have the correct url');
 
@@ -32,26 +32,23 @@ contract('SmartRepository', function(accounts) {
     var penaltyNum = 15;
     var penaltyDenom = 100;
 
-    SmartRepository.new(repoUrl, minCollateral, penaltyNum, penaltyDenom, { from: accounts[0] }).then(function(repo) {
-      var issueUrl = 'https://github.com/foo/bar/issues/1';
-      var issueId = 1;
+    Repository.new(repoUrl, minCollateral, penaltyNum, penaltyDenom, { from: accounts[0] }).then(function(repo) {
+      var issueUrl = 'https://api.github.com/repos/foo/bar/issues/1';
       var amount = web3.toWei(1, 'ether');
 
-      repo.fundIssue(issueUrl, issueId, { from: accounts[1], value: amount }).then(function() {
+      repo.fundIssue(issueUrl, { from: accounts[1], value: amount }).then(function() {
         return repo.getIssueByUrl(issueUrl);
       }).then(function(issue) {
         assert.equal(issueUrl, issue[0], 'issue should have the correct url');
-        assert.equal(issueId, issue[1], 'issue should have the correct id');
-        assert.equal(amount, issue[2], 'issue should have the correct bounty');
+          assert.equal(amount, issue[1], 'issue should have the correct bounty');
 
-        repo.fundIssue(issueUrl, issueId, { from: accounts[2], value: amount }).then(function() {
+        repo.fundIssue(issueUrl, { from: accounts[2], value: amount }).then(function() {
           return repo.getIssueByUrl(issueUrl);
         }).then(function(updatedIssue) {
           var updatedAmount = web3.toWei(2, 'ether');
 
           assert.equal(issueUrl, updatedIssue[0], 'updated issue should have the correct url');
-          assert.equal(issueId, updatedIssue[1], 'updated issue should have the correct id');
-          assert.equal(updatedAmount, updatedIssue[2], 'updated issue should have the correct bounty');
+          assert.equal(updatedAmount, updatedIssue[1], 'updated issue should have the correct bounty');
 
           done();
         }).catch(done);
@@ -65,7 +62,7 @@ contract('SmartRepository', function(accounts) {
     var penaltyNum = 15;
     var penaltyDenom = 100;
 
-    SmartRepository.new(repoUrl, minCollateral, penaltyNum, penaltyDenom, { from: accounts[0] }).then(function(repo) {
+    Repository.new(repoUrl, minCollateral, penaltyNum, penaltyDenom, { from: accounts[0] }).then(function(repo) {
       var postedCollateral = web3.toBigNumber(web3.toWei(1, 'ether')).toNumber();
 
       repo.registerDev({ from: accounts[1], value: postedCollateral }).then(function() {
@@ -74,38 +71,6 @@ contract('SmartRepository', function(accounts) {
         assert.equal(collateral, postedCollateral, 'stored collateral should match posted collateral');
 
         done();
-      }).catch(done);
-    }).catch(done);
-  });
-
-  it('should submit a commit from a registered developer', function(done) {
-    var repoUrl = 'https://github.com/foo/bar';
-    var minCollateral = web3.toBigNumber(web3.toWei(1, 'ether')).toNumber();
-    var penaltyNum = 15;
-    var penaltyDenom = 100;
-
-    SmartRepository.new(repoUrl, minCollateral, penaltyNum, penaltyDenom, { from: accounts[0] }).then(function(repo) {
-      var issueUrl = 'https://github.com/yondonfu/21MarketplaceCrawler/issues/1';
-      var issueId = 1;
-      var amount = web3.toWei(1, 'ether');
-
-      repo.fundIssue(issueUrl, issueId, { from: accounts[1], value: amount }).then(function() {
-        var postedCollateral = web3.toBigNumber(web3.toWei(1, 'ether')).toNumber();
-
-        repo.registerDev({ from: accounts[2], value: postedCollateral }).then(function() {
-          var commitHash = '3080c5761abc2cc7f9e5581bcbbc0fe4961';
-          var commitUrl = 'json(https://api.github.com/repos/yondonfu/21MarketplaceCrawler/commits/3080c5761abc2cc7f9e5581bcbbc0fe4961).sha';
-          var initialBalance = web3.eth.getBalance(repo.address);
-
-          repo.submitCommit(commitHash, issueUrl, commitUrl, { from: accounts[2] }).then(function() {
-            setTimeout(function() {
-              var updatedBalance = web3.eth.getBalance(repo.address);
-              var balanceDiff = initialBalance.minus(updatedBalance);
-
-              assert.equal(balanceDiff, amount, 'contract balance difference should be issue bounty value');
-            }, 8000);
-          }).catch(done);
-        }).catch(done);
       }).catch(done);
     }).catch(done);
   });
